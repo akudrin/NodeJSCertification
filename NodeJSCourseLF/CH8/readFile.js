24009eeb64f8834be652be4a6f4fd8dc93b59d54
Andrei Kudrin <andreikudrin@gmail.com>
2021-05-11 10:21:53 +0100

    NodeJSCourseLF - Callbacks Sync and Async Functions

---
+++ NodeJSCourseLF/CH8/readFile.js
@@ -0,0 +1,10 @@
+const { readFile } = require('fs');
+readFile(__filename, (err, contents) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(contents.toString());
+});
+
+console.log(__filename);

12bfd95de1e7df53d3babb747ecdaf412da53a1f
Andrei Kudrin <andreikudrin@gmail.com>
2021-05-11 13:27:03 +0100

    NodeJSCourseLF - Promise Intro

---
+++ NodeJSCourseLF/CH8/promise1.js
@@ -0,0 +1,21 @@
+function myAsyncOperation(cb) {
+  doSomethingAsynchronous((err, value) => {
+    cb(err, value);
+  });
+}
+
+myAsyncOperation(functionThatHandlesTheResult);
+
+//the same in promise form:
+
+function myAsyncOperation() {
+  return new Promise((resolve, reject) => {
+    doSomethingAsynchronous((err, value) => {
+      if (err) reject(err);
+      else resolve(value);
+    });
+  });
+}
+
+const promise = myAsyncOperation();
+// next up: do something with promise
